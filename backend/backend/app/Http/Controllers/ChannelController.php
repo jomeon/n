5008d81339efcdd0f1fa105d25f0a19e5d16db64
jomeon <szymon.dyrcz1@gmail.com>
2023-07-08 13:58:34 +0200

    backend connecting database to frontend

--- backend/backend/app/Http/Controllers/ChannelController.php
+++ backend/backend/app/Http/Controllers/ChannelController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Channel;
 use Illuminate\Http\Request;
 
 class ChannelController extends Controller
 {
-    //
+    public function index()
+    {
+        $channels = Channel::all();
+        return response()->json($channels);
+    }
+
+    public function store(Request $request)
+    {
+        $channel = new Channel();
+        $channel->name = $request->input('source');
+        $channel->clients = $request->input('customers');
+        $channel->save();
+
+        return response()->json($channel, 201);
+    }
+
+    public function destroy($id)
+    {
+        Channel::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
 }
--- backend/backend/routes/api.php
+++ backend/backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ChannelController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
+    Route::get('/channels', [ChannelController::class, 'index']);
+    Route::post('/channels', [ChannelController::class, 'store']);
+    Route::delete('/channels/{id}', [ChannelController::class, 'destroy']);
 });
